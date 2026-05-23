345ad4d9018320caf1ec7abab5d55b93b96ab716
Om Yadav <om@Oms-MacBook-Air-2.local>
2026-05-23 14:43:57 +0530

    feat: Force HTTPS in production and update services cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\Mail;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production / behind load balancers
+        if (env('APP_ENV') === 'production' || (env('APP_URL') && str_starts_with(env('APP_URL'), 'https://'))) {
+            URL::forceScheme('https');
+        }
+
         Mail::extend('brevo', function (array $config) {
             return new \App\Mail\BrevoApiTransport(env('MAIL_PASSWORD') ?: '');
         });
